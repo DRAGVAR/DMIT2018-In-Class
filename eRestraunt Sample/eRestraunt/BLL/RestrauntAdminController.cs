713b141e27ae008c44c30016d85a1e3793cd298d
DRAGVAR <dragvar@shaw.ca>
2014-09-17 12:33:54 -0600

    Added rest of manages

--- eRestraunt Sample/eRestraunt/BLL/RestrauntAdminController.cs
+++ eRestraunt Sample/eRestraunt/BLL/RestrauntAdminController.cs
@@ -39,22 +39,85 @@ namespace eRestraunt.BLL
 
         #region Manage Tables
         #region Command
+        public int AddTable(Table item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTable(Table item)
+        {
+        }
+
+        public void DeleteTable(Table item)
+        {
+        }
         #endregion
         #region Query
+        public List<Table> ListAllTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Table GetTable(int tableID)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
         #endregion
 
         #region Manage Items
         #region Command
+        public int AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+        }
+
+        public void DeleteItem(Item item)
+        {
+        }
         #endregion
         #region Query
+        public List<Item> ListAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(int itemID)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
         #endregion
 
         #region Manage Special Events
         #region Command
+        public int AddSpecialEvent(SpecialEvent item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSpecialEvent(SpecialEvent item)
+        {
+        }
+
+        public void DeleteSpecialEvent(SpecialEvent item)
+        {
+        }
         #endregion
         #region Query
+        public List<SpecialEvent> ListAllSpecialEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SpecialEvent GetSpecialEvent(int specialeventID)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
         #endregion
     }
