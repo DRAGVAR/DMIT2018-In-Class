ec75cb2fc0932ca006cada96ff6c4a10d63f8335
DRAGVAR <dragvar@shaw.ca>
2014-09-23 14:23:42 -0600

    Added Dataobjectreaders

--- eRestraunt Sample/eRestraunt/BLL/RestrauntAdminController.cs
+++ eRestraunt Sample/eRestraunt/BLL/RestrauntAdminController.cs
@@ -6,12 +6,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Usings
+    using System.ComponentModel;
+#endregion
+
 namespace eRestraunt.BLL
 {
+    [DataObject]
     public class RestrauntAdminController
     {
         #region Manage Waiters
         #region Command
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public int AddWaiter(Waiter item)
         {
             using (RestrauntContext context = new RestrauntContext())
@@ -23,6 +29,7 @@ namespace eRestraunt.BLL
             }
         }
 
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
         public void UpdateWaiter(Waiter item) 
         {
             using (RestrauntContext context = new RestrauntContext())
@@ -35,6 +42,7 @@ namespace eRestraunt.BLL
             }
         }
 
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
         public void DeleteWaiter(Waiter item)
         {
             using (RestrauntContext context = new RestrauntContext())
@@ -46,6 +54,7 @@ namespace eRestraunt.BLL
         }
         #endregion
         #region Query
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<Waiter> ListAllWaiters()
         {
             using (RestrauntContext context = new RestrauntContext())
@@ -54,6 +63,7 @@ namespace eRestraunt.BLL
             }
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public Waiter GetWaiter(int waiterID)
         {
             using (RestrauntContext context = new RestrauntContext())
@@ -66,11 +76,13 @@ namespace eRestraunt.BLL
 
         #region Manage Tables
         #region Command
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public int AddTable(Table item)
         {
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
         public void UpdateTable(Table item)
         {
         }
@@ -94,25 +106,30 @@ namespace eRestraunt.BLL
 
         #region Manage Items
         #region Command
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public int AddItem(Item item)
         {
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
         public void UpdateItem(Item item)
         {
         }
 
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
         public void DeleteItem(Item item)
         {
         }
         #endregion
         #region Query
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<Item> ListAllItems()
         {
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public Item GetItem(int itemID)
         {
             throw new NotImplementedException();
@@ -122,25 +139,30 @@ namespace eRestraunt.BLL
 
         #region Manage Special Events
         #region Command
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public int AddSpecialEvent(SpecialEvent item)
         {
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
         public void UpdateSpecialEvent(SpecialEvent item)
         {
         }
 
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
         public void DeleteSpecialEvent(SpecialEvent item)
         {
         }
         #endregion
         #region Query
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<SpecialEvent> ListAllSpecialEvents()
         {
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
         public SpecialEvent GetSpecialEvent(int specialeventID)
         {
             throw new NotImplementedException();
