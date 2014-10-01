11ab9f3ace343c34686fa8e901fef8f919e98960
DRAGVAR <dragvar@shaw.ca>
2014-10-01 14:03:42 -0600

    Changed Item.cs Entity Class

--- eRestraunt Sample/eRestraunt/Entities/Item.cs
+++ eRestraunt Sample/eRestraunt/Entities/Item.cs
@@ -4,22 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Usings
+using System.ComponentModel.DataAnnotations;
+#endregion
+
 namespace eRestraunt.Entities
 {
     #region Item
     public class Item
     {
+        public Item()
+        {
+            Active = true;
+        }
+
+        [Key]
         public int ItemID { get; set; }
         public string Description { get; set; }
         public decimal CurrentPrice { get; set; }
         public decimal CurrentCost { get; set; }
         public bool Active { get; set; }
-        public int Calories { get; set; }
+        public int? Calories { get; set; }
         public string Comment { get; set; }
         public int MenuCategoryID { get; set; }
 
         #region Navigation Properties
-        public virtual ICollection<BillItem> BillItems { get; set; }
+        public virtual MenuCategory Category { get; set; }
         #endregion
     }
     #endregion
