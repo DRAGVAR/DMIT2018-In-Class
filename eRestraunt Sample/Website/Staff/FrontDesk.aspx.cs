4db2bf04e3f7d5bfd3438a8c41fea999b736b895
DRAGVAR <dragvar@shaw.ca>
2014-10-23 09:55:36 -0600

    Added ClockPicker And Updated FrontDesk.aspx

--- eRestraunt Sample/Website/Staff/FrontDesk.aspx.cs
+++ eRestraunt Sample/Website/Staff/FrontDesk.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eRestraunt.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,5 +11,21 @@ public partial class Staff_FrontDesk : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
+    {
+        MessageUserControl.TryRun(SetMockedTimeToLastBill);
+    }
+    
+    private void SetMockedTimeToLastBill()
+    {
+        var controller = new AdHocController();
+        var info = controller.GetLastBillDateTime();
+        // Formatting date for use in an <input type="date"> HTML5 control
+        SearchDate.Text = info.ToString("yyyy-MM-dd");
+
+        // Formatting time for use in an <input type="time"> HTML5 control
+        SearchTime.Text = info.ToString("HH:mm:ss"); // HH is the 24 hour clock, hh is the 12 hour clock
     }
 }
---
+++ eRestraunt Sample/eRestraunt/BLL/AdHocController.cs
@@ -0,0 +1,24 @@
+﻿using eRestraunt.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestraunt.BLL
+{
+    [DataObject]
+    public class AdHocController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public DateTime GetLastBillDateTime()
+        {
+            using (var context = new RestrauntContext())
+            {
+                var result = context.Bills.Max(x => x.BillDate);
+                return result;
+            }
+        }
+    }
+}
